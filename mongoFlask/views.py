7ee84b71cf06b0e6565d27097d4f5354bf86efce
Cevaris <cevaris@gmail.com>
2014-02-17 19:13:50 -0700

    got search working

--- mongoFlask/views.py
+++ mongoFlask/views.py
@@ -9,36 +9,12 @@ articles = Blueprint('articles', __name__, template_folder='templates')
 
 class Search(MethodView):
 
-  form = model_form(Article, exclude=['created_at', 'slug'])
-
-  def get_context(self, slug):
-    article = Article.objects.get_or_404(slug=slug)
-    form = self.form(request.form)
-
-    context = {
-      "article": article,
-      "form": form
-    }
-    return context
-
-  def get(self, slug):
-    context = self.get_context(slug)
-    return render_template('article/show.html', **context)
-
-  def post(self, slug):
-      context = self.get_context(slug)
-      form = context.get('form')
-
-      if form.validate():
-        comment = Comment()
-        form.populate_obj(comment)
-
-        article = context.get('article')
-        article.slug = slugify(article.title)
-        article.save()
-
-        return redirect(url_for('articles.show', slug=slug))
-      return render_template('articles/show.html', **context)
+  def get(self, query):
+    try:
+      articles = Article.objects(body__contains=query)
+    except Article.DoesNotExist:
+      articles = []
+    return render_template('articles/index.html', articles=articles)
 
 class Index(MethodView):  
 
@@ -55,6 +31,6 @@ class ShowArticle(MethodView):
 
 
 
-articles.add_url_rule('/', view_func=Index.as_view('index'))
-articles.add_url_rule('/search/', view_func=Index.as_view('search'))
+articles.add_url_rule('/articles', view_func=Index.as_view('index'))
+articles.add_url_rule('/articles/search/<query>', view_func=Search.as_view('search'))
 articles.add_url_rule('/articles/<slug>/', view_func=ShowArticle.as_view('show'))
